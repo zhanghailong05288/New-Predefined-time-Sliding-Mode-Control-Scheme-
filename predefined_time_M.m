95db9f6f9937166717645fb2956658b48d22654c
zhanghailong05288 <40680825+zhanghailong05288@users.noreply.github.com>
2025-02-24 15:13:38 +0800

    Add files via upload
    Simulink and Matlab code.

---
+++ predefined_time_M.m
@@ -0,0 +1,20 @@
+% ?????????????
+order= 0.95;
+
+Tc = 0.2;
+n =12;
+
+
+phi_1 = (n*pi)/(4*Tc);
+phi_2 = pi/(4*Tc);
+phi_3 = (n*pi)/(4*Tc);
+phi_4 =  pi/(4*Tc);
+
+% q=0.25;
+% % 做arctanx的论文实验
+% phi_1 = pi/(q*Tc);
+% phi_2 = pi/(q*Tc);
+% phi_3 = pi/(q*Tc);
+% phi_4 = pi/(q*Tc);
+% phi_5 = 1-q/2;
+% phi_6 = 1+q/2;
